1e61bc7b7ad704b3936afc3e1d6526b06f9a8d5e
preussju <jveiga@byupathway.edu>
2025-06-30 18:16:39 -0300

    finished 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {second}, {first} {second}.");
     }
 }

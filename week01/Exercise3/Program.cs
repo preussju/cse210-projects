ed5c8035972740980f224dd6f096f5af31df80cd
preussju <jveiga@byupathway.edu>
2025-07-04 13:32:08 -0300

    finished 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guessNumber = 0;
+
+         while (guessNumber != magicNumber)
+        {
+            Console.Write("Enter yout guess: ");
+            string guessInput = Console.ReadLine();
+            guessNumber = int.Parse(guessInput);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber == magicNumber)
+            {
+                Console.Write("You guessed it!");
+            }
+        }
     }
 }

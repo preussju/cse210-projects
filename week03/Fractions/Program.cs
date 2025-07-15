96007e9031ff690e151b98268c5db5b6ce5eb8a3
preussju <jveiga@byupathway.edu>
2025-07-15 14:45:57 -0300

    encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+        Fraction f4 = new Fraction();
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+
+        Console.Write("Top: ");
+        int top = int.Parse(Console.ReadLine());
+        Console.Write("Bottom: ");
+        int bottom = int.Parse(Console.ReadLine());
+
+        f4.SetTop(top);
+        f4.SetBottom(bottom);
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // learning to use get and set
+        // learning to use constructors
     }
 }

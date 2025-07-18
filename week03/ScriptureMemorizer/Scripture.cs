03fce68e04df39e5cbf87c4fbb32383730c8027a
preussju <jveiga@byupathway.edu>
2025-07-18 14:26:36 -0300

    stubs

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRandomwords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
+    
+}

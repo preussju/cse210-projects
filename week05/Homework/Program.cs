7abaabb30b0fca739efeff1156d0d943c483abad
preussju <jveiga@byupathway.edu>
2025-07-29 22:16:55 -0300

    finished

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Julia", "Division");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Julie", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Jules", "History", "The WW2");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
+
+//learn and practice the principle of Inheritance

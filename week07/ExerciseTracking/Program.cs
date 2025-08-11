8907a36c7deb3d748e4e2a4bf62b6bc3dee36f1d
preussju <jveiga@byupathway.edu>
2025-08-11 12:29:17 -0300

    finished code

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,8 +5,8 @@ class Program
     static void Main(string[] args)
     {
         RunningActivity running = new RunningActivity("08 AGO 2025", 30, 4.8d);
-        SwimmingActivity swimming = new SwimmingActivity("08 AGO 2025", 30, 10d);
-        CyclingActivity cycling = new CyclingActivity("08 AGO 2025", 30, 12d);
+        SwimmingActivity swimming = new SwimmingActivity("08 AGO 2025", 60, 10d);
+        CyclingActivity cycling = new CyclingActivity("08 AGO 2025", 40, 12d);
 
         List<Activity> activities = new List<Activity>();
         activities.Add(running);
